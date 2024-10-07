e1e37dab9ecc97c7a7d65508778feda69e550633
daidr <daidr@daidr.me>
2024-10-07 23:00:53 +0800

    fix: risk control

--- src/utils/networkHook.ts
+++ src/utils/networkHook.ts
@@ -1,7 +1,7 @@
 import type { AdvancedHeaders } from './advancedFetch'
 import { range } from './utils'
 const targetPages = [
-  'https://api-os-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie?game_biz=hkrpg_global',
+  'https://api-os-takumi.mihoyo.com/binding/api/getUserGameRolesByLtoken?game_biz=hkrpg_global',
   'https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookieToken?game_biz=hkrpg_cn',
   'https://bbs-api-os.hoyolab.com/game_record/app/hkrpg/api/note*',
   'https://api-takumi-record.mihoyo.com/game_record/app/hkrpg/api/note*',

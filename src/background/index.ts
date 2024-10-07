6e2722f5a780cd0186318a362f69459c2d7443b7
daidr <daidr@daidr.me>
2024-10-07 23:03:16 +0800

    feat: release 0.2.2

--- src/background/index.ts
+++ src/background/index.ts
@@ -376,7 +376,7 @@ onMessage<{ uid: string }, 'request_captcha_bg'>(
   async ({ data: { uid } }) => {
     // open captcha tab
     const curtab = await tabs.create({
-      url: 'https://webstatic.mihoyo.com/app/community-game-records/?game_id=6&ref=pompom',
+      url: 'https://webstatic.mihoyo.com/app/community-game-records/rpg/?game_id=6&ref=pompom&mhy_presentation_style=fullscreen#/',
     })
 
     // wait for curtab loaded
